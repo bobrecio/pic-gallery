cc098eba7909a81d45c88740cc4f38073ec9145a
bobrecio <bobrecio@gmail.com>
2022-02-05 16:09:48 -0600

    saving point

--- modal.js
+++ modal.js
@@ -1,3 +1,10 @@
+const picArray = ["pets1.webp","pets3.webp","pets4.webp","pets5.webp","pets6.webp"];
+const gallery = document.getElementById('gallery');
+const pictureContainer = document.getElementById('pictureContainer');
+const thumbsContainer = document.getElementById('thumbsContainer');
+
+setUpGallery;
+
 function openModal() {
     document.getElementById("myModal").style.display = "block";
 }
@@ -36,4 +43,32 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
     captionText.innerHTML = dots[slideIndex - 1].alt;
+}
+
+function setUpGallery(){
+    for (let i = 0; i < picArray.length; i++) {
+        // <div class="mySlides">
+        //     <div class="numbertext">1 / 4</div>
+        //     <img src="images/pets1.webp" style="width:100%">
+        // </div>
+        const mySlides = document.createElement('div');
+        mySlides.classList.add("mySlides");
+
+        const numberText = document.createElement('div');
+        numberText.classList.add("numberText");
+
+        const image = document.createElement('img');
+        image.src = `./images/${picArray[i]}`;
+        image.style.width = "100%";
+
+        mySlides.appendChild(numberText);
+        mySlides.appendChild(image);
+
+
+
+
+        const thumb = document.createElement(div)
+        
+    }
+
 }
