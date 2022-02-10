1692ec7eed1d3e864296aa74e6f0f06f3d2ed430
Recio <recio@ubuntu>
2022-02-10 07:30:53 -0600

    check-in

--- JQQ/app.js
+++ JQQ/app.js
@@ -1,12 +1,15 @@
 const gallery = document.getElementById("gallery");
 const popup = document.getElementById("popup");
-const numbertext = document.getElementById("numbertext");
+const slideNumber = document.getElementById("numbertext");
+const caption = document.getElementById("caption");
 const selectedImage = document.getElementById("selectedImage");
+const btnPrev = document.getElementById("btnPrev");
+const btnNext = document.getElementById("btnNext");
 
-const imageIndexes = [1, 3, 4, 5, 6];
+const imageList = [1, 3, 4, 5, 6];
 const selectedIndex = null;
 
-imageIndexes.forEach((v, i) => {
+imageList.forEach((v, i) => {
   const image = document.createElement('img');
   image.src = `./images/pets${v}.webp`;
   image.alt = `Pet picture ${v}`;
@@ -14,10 +17,21 @@ imageIndexes.forEach((v, i) => {
   //image.classList.add('mySlides');
   image.addEventListener('click', () => {
     selectedImage.src = `./images/pets${v}.webp`;
-    numbertext.innerText = `${v}/${imageIndexes.length}`;
+    slideNumber.innerText = `${i+1}/${imageList.length}`;
     popup.style.transform = `translateY(0)`;
   });
   gallery.appendChild(image);
+})
+
+btnPrev.addEventListener('click', () => {
+  let thisSlide = slideNumber.innerText.split("/")[0];
+  selectedImage.src = `./images/pets${thisSlide-1}.webp`;
+  slideNumber.innerText = `${thisSlide-1}/${imageList.length}`;
+})
+btnNext.addEventListener('click', () => {
+  let thisSlide = slideNumber.innerText.split("/")[0];
+  selectedImage.src = `./images/pets${thisSlide-1}.webp`;
+  slideNumber.innerText = `${thisSlide-1}/${imageList.length}`;
 })
 
 popup.addEventListener('click', () => {
@@ -34,35 +48,35 @@ const closeModal = () => {
   document.getElementById("myModal").style.display = "none";
 }
 
-let slideIndex = 1;
-showSlides(slideIndex);
+// let slideIndex = 1;
+// showSlides(slideIndex);
 
-const changeSlide = (n) => {
-  showSlides(slideIndex += n);
-}
+// const changeSlide = (n) => {
+//   showSlides(slideIndex += n);
+// }
 
-const currentSlide = (n) => {
-  showSlides(slideIndex = n);
-}
+// const currentSlide = (n) => {
+//   showSlides(slideIndex = n);
+// }
 
-const showSlides = (n) => {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {
-    slideIndex = 1
-  }
-  if (n < 1) {
-    slideIndex = slides.length
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-  captionText.innerHTML = dots[slideIndex - 1].alt;
-}
+// const showSlides = (n) => {
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
+//   var dots = document.getElementsByClassName("demo");
+//   var captionText = document.getElementById("caption");
+//   if (n > slides.length) {
+//     slideIndex = 1
+//   }
+//   if (n < 1) {
+//     slideIndex = slides.length
+//   }
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex - 1].style.display = "block";
+//   dots[slideIndex - 1].className += " active";
+//   captionText.innerHTML = dots[slideIndex - 1].alt;
+// }
