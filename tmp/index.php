f609beea4d7faf3e98bd20a2e8e8a9d92df6d388
Recio <recio@ubuntu>
2022-02-12 11:36:20 -0600

    added old versions in /tmp

---
+++ tmp/index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Photo Gallery</title>
+    <link rel="stylesheet" href="./style.css" />
+</head>
+
+<body>
+    <?php
+    $place = strtolower(empty($_GET["loc"])) ? "." : ($_GET["loc"]);
+    $dir = strtolower(empty($_GET["loc"])) ? "." : ($_GET["loc"]);
+    //$place = "./images";
+    //$dir = "./images";
+    $files = scandir($dir);
+
+    $galleryImages = "[]";
+    $galleryDirs = "[]";
+    $galleryNav = "";
+
+    foreach ($files as $file) {
+        $path = $dir . "/" . $file;
+        $galleryNav = dirname($path);
+
+        if (is_file($path) && (strtolower(substr($path, -3)) == 'png' || strtolower(substr($path, -3)) == 'jpg' || strtolower(substr($path, -4)) == 'webp' || strtolower(substr($path, -4)) == 'jpeg')) {
+            $galleryImagesIsEmpty = ($galleryImages != "[]") ? false : true;
+            $comma = $galleryImagesIsEmpty ? "" : ",";
+            $addNewFile = $comma."'".$file."']";
+            $galleryImages = substr_replace($galleryImages, $addNewFile, -1);
+        } elseif (is_dir($path) && $file != "." && $file != "..") {
+            $galleryDirsIsEmpty = ($galleryDirs != "[]") ? false : true;
+            $comma = $galleryDirsIsEmpty ? "" : ",";
+            $addNewFile = $comma."'".$file."']";
+            $galleryDirs = substr_replace($galleryDirs, $addNewFile, -1);
+        } // elseif ($file == "..") {
+        //     $strLine = "";
+        //     $strLine = $strLine . '<a href="./index.php?loc=' . dirname(dirname($path)) . '" ';
+        //     $strLine = $strLine . ' target="_self">';
+        //     $strLine = $strLine . '<h1>' . '[/' . dirname(dirname($path)) . ']' . str_replace("./", " ", $_GET["loc"]) . '</h1>';
+        //     $strLine = $strLine . '</a>';
+        //     echo $strLine;
+        // }
+    }
+    echo "<script>";
+    echo "\nlet galleryImages = " . $galleryImages . "; ";
+    echo "\nlet galleryDirs = " . $galleryDirs . "; ";
+    echo "\nlet galleryNav = '" . $galleryNav . "'; ";
+    echo "\nlet galleryParent = '" . dirname($galleryNav) . "'; ";
+    echo "\n</script>";
+    ?>
+    <div id="header">
+        <a id='gotoParent'>&nbsp;&lArr;&nbsp;back to&nbsp;</a>    
+        <div id='pathTitle'></div>
+        <div id='subFolders'></div>
+    </div> 
+    <div class='container'>
+        <div id="gallery"></div>
+    </div>
+    <div id="popup">
+        <span class="close cursor" onclick="closeModal()">&times;</span>
+        <div class="modal-content">
+            <a id="btnPrev">&#10094;</a>
+            <div id="slideNumber"></div>
+            <img src="" alt="" id="selectedImage">
+            <a id="btnNext">&#10095;</a>
+            <div class="caption-container">
+                <p id="caption"></p>
+            </div>
+        </div>
+    </div>
+    <script src="./script.js"></script>
+</body>
+
+</html>
---
+++ tmp/script.js
@@ -0,0 +1,71 @@
+const gallery = document.getElementById("gallery");
+const popup = document.getElementById("popup");
+const slideNumber = document.getElementById("slideNumber");
+const caption = document.getElementById("caption");
+const selectedImage = document.getElementById("selectedImage");
+const btnPrev = document.getElementById("btnPrev");
+const btnNext = document.getElementById("btnNext");
+const pathTitle = document.getElementById("pathTitle");
+const gotoParent = document.getElementById("gotoParent");
+const subFolders = document.getElementById("subFolders");
+
+const imageList = galleryImages;
+const dirList = galleryDirs;
+
+const selectedIndex = null;
+
+gotoParent.href = "./index.php?loc=" + galleryParent;
+gotoParent.append(galleryParent);
+
+pathTitle.textContent = galleryNav;
+
+dirList.forEach ((dir,i) => {
+  const dirLink = document.createElement("a");
+  let subURL = "./index.php?loc=" + galleryNav + "/" + dir;
+  dirLink.href = subURL;
+  dirLink.text = dir;
+  dirLink.classList.add('subFolder');
+  subFolders.appendChild(dirLink);
+})
+
+imageList.forEach((v, i) => {
+  const image = document.createElement('img');
+  image.src = `${pathTitle.textContent}/${v}`;
+  image.alt = `${v}`;
+  image.classList.add('galleryImg');
+
+  image.addEventListener('click', () => {
+    selectedImage.src = `${pathTitle.textContent}/${v}`;
+    slideNumber.innerText = `${i+1}/${imageList.length}`;
+    popup.style.transform = `translateY(0)`;
+    caption.innerText = `${pathTitle.textContent}/${v}`;
+
+  });
+  gallery.appendChild(image);
+})
+
+btnPrev.addEventListener('click', () => {
+  let thisSlide = slideNumber.innerText.split("/")[0];
+  selectedImage.src = `${thisSlide-1}`;
+  slideNumber.innerText = `${thisSlide-1}/${imageList.length}`;
+})
+
+btnNext.addEventListener('click', () => {
+  let thisSlide = slideNumber.innerText.split("/")[0];
+  selectedImage.src = `${thisSlide-1}`;
+  slideNumber.innerText = `${thisSlide-1}/${imageList.length}`;
+})
+
+popup.addEventListener('click', () => {
+  popup.style.transform = `translateX(-100%)`;
+  popup.src = '';
+  popup.alt = '';
+})
+
+const openModal = () => {
+  document.getElementById("myModal").style.display = "block";
+}
+
+const closeModal = () => {
+  document.getElementById("myModal").style.display = "none";
+}
